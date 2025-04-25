625c3ae533a000a88949ac26c4f6b374f0369c05
Dhanush Ram Suresh <dhanush@qonsult.systems>
2025-04-25 15:59:30 +0530

    changes

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -18,8 +18,8 @@ public class HomeController : Controller
         home ohome = new home();
 
         CommonServices commonServices = new CommonServices();
-      ohome.homeparam= commonServices.PostRequest("", "http://localhost:5000/WeatherForecast");
-        ohome.autoparam = commonServices.PostRequest("", "http://localhost:5000/autocad");
+      ohome.homeparam= commonServices.PostRequest("", "http://host.docker.internal:5000/WeatherForecast");
+        ohome.autoparam = commonServices.PostRequest("", "http://host.docker.internal:5000/autocad");
         return View(ohome);
     }
 
